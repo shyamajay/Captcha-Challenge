d70dc2fc903795d421ce8a953cf50160d831aa19
shyamajay <nehrushyam12@gmail.com>
2022-12-25 15:06:27 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {random} from 'faker';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  
   title = 'typing';
+  sentence = '';
+  enteredNumber='';
+  length=0;
+  
+  ngOnInit(): void {
+    this.sentence = random.number().toString();
+    this.length = this.sentence.length;
+  }
+  
+  checkCharacter(letter:any,enteredNumber:any){
+
+
+
+    if(!enteredNumber){
+      return 'pending';
+    }
+
+    return letter === enteredNumber ? 'correct' : 'wrong';
+  }
+
+  onEnterValue(input:any){
+    // console.log(input.value);
+    this.enteredNumber = input.value;
+    console.log(this.enteredNumber);
+  }
 }
